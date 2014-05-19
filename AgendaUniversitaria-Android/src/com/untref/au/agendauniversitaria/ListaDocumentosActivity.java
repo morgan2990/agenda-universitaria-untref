aa41c993c4a04857002a1bfea8032cbba4b4eee0
Rodrigo Morganti <morgan2990@gmail.com>
2014-05-19 14:11:34 -0300

    renombrar el proyecto android

---
+++ AgendaUniversitaria-Android/src/com/untref/au/agendauniversitaria/ListaDocumentosActivity.java
@@ -0,0 +1,19 @@
+package com.untref.au.agendauniversitaria;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.untref.au.agendauniversitaria.MainActivity.PlaceholderFragment;
+
+public class ListaDocumentosActivity extends Activity {
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_lista_documentos);
+		
+	}
+
+}
